2c52b378aa1a0ab2dddd37e4cbd5a17fdcd2098d
otaku9820 <aguido@unal.edu.co>
2023-08-29 00:54:51 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -55,10 +55,10 @@ public class Control {
 		this.tv=tv;
 		tv.setControl(this);
 	}
-	public void setTV(TV tv) {
+	public void setTv(TV tv) {
 		this.tv=tv;
 	}
-	public TV getTV(TV tv) {
+	public TV getTv(TV tv) {
 		return this.tv;
 	}
 
